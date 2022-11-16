a089ab211cf4539ea9a711191d4867545d98cdd6
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-16 15:40:37 +0100

    Ciclo for

--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            message: ''
+            emails: [],
         }
     },
-    methods: {
-
-    },
-    created(){       
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-            // console.log(this);
-            // console.log(response.data.response);
-            this.message = response.data.response;
-        })
+    created(){  
+        for(let i = 1; i <= 10; i++) {     
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(this);
+                // console.log(response.data.response);
+                this.emails.push(response.data.response);
+            });
+        }
     }
 }).mount('#app')
